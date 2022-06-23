d63850cd85a0f88874231940aaca68ad2baa1759
stkch0832 <stkch0832@gmail.com>
2022-06-23 21:56:51 +0900

    create templates
    create base.html
    create index.html
    create style.css

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
+from django.views.generic import View
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+class IndexView(View):
+    def get (self, request, *args, **kwargs):
+        post_data = Post.objects.order_by('-id')
+        return render(request, 'app/index.html', {'post_data': post_data})
